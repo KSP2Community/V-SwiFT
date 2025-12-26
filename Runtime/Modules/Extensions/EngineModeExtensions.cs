04de77dd5f64d28f23855d2dd922c551e0ecb30e
Jan Bureš <jan.bures@live.com>
2025-12-26 17:25:39 +0100

    Use PhysicsSettings instead of magical numbers

--- Runtime/Modules/Extensions/EngineModeExtensions.cs
+++ Runtime/Modules/Extensions/EngineModeExtensions.cs
@@ -7,17 +7,17 @@ namespace VSwift.Modules.Extensions
     {
         public static float GetMinFuelFlow(this Data_Engine.EngineMode currentMode)
         {
-            return (float)(currentMode.minThrust / (currentMode.atmosphereCurve.Evaluate(0f) * 9.80665));
+            return (float)(currentMode.minThrust / (currentMode.atmosphereCurve.Evaluate(0f) * PhysicsSettings.STANDARD_GRAVITY_EARTH));
         }
 
         public static float GetMaxFuelFlow(this Data_Engine.EngineMode currentMode)
         {
-            return (float)(currentMode.maxThrust / (currentMode.atmosphereCurve.Evaluate(0f) * 9.80665));
+            return (float)(currentMode.maxThrust / (currentMode.atmosphereCurve.Evaluate(0f) * PhysicsSettings.STANDARD_GRAVITY_EARTH));
         }
-    
+
         public static float GetThrust(this Data_Engine.EngineMode currentMode, float atmPressure, float thrustLevel)
         {
-            return Mathf.Lerp(currentMode.GetMinFuelFlow(), currentMode.GetMaxFuelFlow(), thrustLevel) * (currentMode.atmosphereCurve.Evaluate(atmPressure) * 9.80665f);
-        } 
+            return Mathf.Lerp(currentMode.GetMinFuelFlow(), currentMode.GetMaxFuelFlow(), thrustLevel) * (currentMode.atmosphereCurve.Evaluate(atmPressure) * (float)PhysicsSettings.STANDARD_GRAVITY_EARTH);
+        }
     }
 }
